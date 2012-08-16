5d9fbff8e6f602f54f7d5d2d643c524cc02e4686
TedLiang <teddyliang@gmail.com>
2012-08-16 22:38:28 +1000

    implement customizable method name via data-jquery-formkit-config attribute in script tag. fix data value issue when its type is number

--- src/jquery.formkit.js
+++ src/jquery.formkit.js
@@ -10,109 +10,111 @@
  */
 (function($){
 
-	$.fn.extend({
-
-		extractForm: function ( options ) {
-			if(typeof options == 'string') options = { format: options };
-			else if($.isFunction(options)) options = { buildObject: options };
-
-			var settings = formkit.getSettings(options);
-			switch (settings.format.toLowerCase()) {
-				case 'array':
-					return form.toArray(form.enabledElements(this), settings.buildArray);
-				case 'query':
-					return $.param(form.toObject(form.enabledElements(this), settings.buildObject));
-				default:
-					return form.toObject(form.enabledElements(this), settings.buildObject);
-			}
-		},
-
-		fillForm: function( data ) {
-			if ( typeof data == 'string' ) {
-				var param, data = formkit.arrayToObject($.map(data.split( "&" ), function(val){
-					param = val.split( "=" );
-					return { name: decodeURIComponent( param[0] ),
-							value: decodeURIComponent( param[1].replace( rPlus, "%20" )) };
-				}));
-			}
-			else if ( $.isArray( data ) ) {
-				data = formkit.arrayToObject(data);
-			}
-			return form.fromObject(form.elements(this), data);
-		},
-
-		copyForm: function( target ) {
-			return ((!target || typeof target == 'string') ? $(target) :
-						target).fillForm(this.extractForm());
-		},
-
-		resetForm: function() {
-			this.each(function() { this.reset(); });
-			return this;
-		},
-
-		extractField: function( name ) {
-			if(name){
-				return form.toObject(form.enabledElements(this, name), formkit.settings.buildObject)[name];
-			}
-			else{
-				var ret = this.extractForm(), count = 0, attr;
-				for (k in ret) if (ret.hasOwnProperty(k)) {attr=k, count++;}
-				switch (count) {
-					case 0: return undefined;
-					case 1: return ret[attr];
-					default: return ret;
-				}
-			}
-		},
-
-		fillField: function( name, value ) {
-			if (value==undefined) {
-				if (this.length==0) return $();
-				value = name;
-				name = this[0].name;
-			}
-			var obj = {};
-			obj[name]=value;
-			return form.fromObject(form.elements(this, name), obj);
-		},
-
+	var formkit = {
+		format: 'object',
+		extractForm: 'extractForm',
+		fillForm: 'fillForm',
+		copyForm: 'copyForm',
+		resetForm: 'resetForm',
+		extractField: 'extractField',
+		fillField: 'fillField',
+	}, configNv, configVal;
+	
+	$.each($('script[data-jquery-formkit-config]').filter(':first').attr(
+			'data-jquery-formkit-config').split(';'), function(){
+		configNv = this.split(':');
+		configVal = $.trim(configNv[1]);
+		if(configVal) formkit[$.trim(configNv[0])] = configVal;
 	});
 
-	var formkit = {
-		settings : {
-			format: 'object',
+	$.fn[formkit.extractForm] = function ( options ) {
+		if(typeof options == 'string') options = { format: options };
+		else if($.isFunction(options)) options = { buildObject: options };
 
-			buildObject: function(/*String*/value, /*Object*/obj){
-				var name = this.name, val = obj[name];
-				if(typeof val == "string"){
-					obj[name] = [val, value];
-				}else if($.isArray(val)){
-					val.push(value);
-				}else{
-					obj[name] = value;
-				}
-			},
-
-			buildArray: function(/*String*/value){
-				return {name: this.name, value: value};
-			},
-
-		},
-
-		getSettings: function(options){
-			options = options || {};
-			return $.extend({}, formkit.settings, options);
-		},
-
-		arrayToObject: function(array){
-			var ret = {};
-			$.each(array, function(){
-				formkit.settings.buildObject.call(this, this.value, ret);
-			});
-			return ret;
+		var settings = $.extend({}, defaultFn, formkit, (options || {}));
+		switch (settings.format.toLowerCase()) {
+			case 'array':
+				return form.toArray(form.enabledElements(this), settings.buildArray);
+			case 'query':
+				return $.param(form.toObject(form.enabledElements(this), settings.buildObject));
+			default:
+				return form.toObject(form.enabledElements(this), settings.buildObject);
 		}
+	};
 
+	$.fn[formkit.fillForm] = function( data ) {
+		if ( typeof data == 'string' ) {
+			var param, data = arrayToObject($.map(data.split( "&" ), function(val){
+				param = val.split( "=" );
+				return { name: decodeURIComponent( param[0] ),
+						value: decodeURIComponent( param[1].replace( rPlus, "%20" )) };
+			}));
+		}
+		else if ( $.isArray( data ) ) {
+			data = arrayToObject(data);
+		}
+		return form.fromObject(form.elements(this), data);
+	};
+
+	$.fn[formkit.copyForm] = function( target ) {
+		return ((!target || typeof target == 'string') ? $(target) :
+					target)[formkit.fillForm](this[formkit.extractForm]());
+	};
+
+	$.fn[formkit.resetForm] = function() {
+		this.each(function() { this.reset(); });
+		return this;
+	};
+
+	$.fn[formkit.extractField] = function( name ) {
+		if(name){
+			return form.toObject(form.enabledElements(this, name), defaultFn.buildObject)[name];
+		}
+		else{
+			var ret = this[formkit.extractForm](), count = 0, attr;
+			for (k in ret) if (ret.hasOwnProperty(k)) {attr=k, count++;}
+			switch (count) {
+				case 0: return undefined;
+				case 1: return ret[attr];
+				default: return ret;
+			}
+		}
+	};
+
+	$.fn[formkit.fillField] = function( name, value ) {
+		if (value==undefined) {
+			if (this.length==0) return $();
+			value = name;
+			name = this[0].name;
+		}
+		var obj = {};
+		obj[name]=value;
+		return form.fromObject(form.elements(this, name), obj);
+	};
+
+	var	defaultFn = {
+		buildObject: function(/*String*/value, /*Object*/obj){
+			var name = this.name, val = obj[name];
+			if(typeof value == "number") value = value.toString();
+			if(typeof val == "string"){
+				obj[name] = [val, value];
+			}else if($.isArray(val)){
+				val.push(value);
+			}else{
+				obj[name] = value;
+			}
+		},
+		buildArray: function(/*String*/value){
+			return {name: this.name, value: value};
+		},
+	};
+
+	var arrayToObject = function(array){
+		var ret = {};
+		$.each(array, function(){
+			defaultFn.buildObject.call(this, this.value, ret);
+		});
+		return ret;
 	};
 
 	var rcheck = /^(radio|checkbox)$/i, rCRLF = /\r?\n/g, rPlus = /\+/g,
@@ -196,7 +198,9 @@
 			var $node = $(inputNode);
 			if(rcheck.test(inputNode.type)){
 				var prop = (inputNode.value == value ||
-						($.isArray(value) && $.inArray(inputNode.value, value)!=-1));
+							($.isArray(value) && 
+							($.inArray(inputNode.value, value)!=-1 ||
+							$.inArray(parseFloat(inputNode.value), value)!=-1)));
 				if($node.prop('checked')!=prop){
 					$node.prop('checked', prop);
 					return true;
